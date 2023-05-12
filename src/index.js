bedfdd759aa4f9bdb38704d79b1591fd8d3ca12b
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-05-12 21:16:08 +0700

    new imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { Router } from '@vaadin/router';
 import { NavElement } from '../components/nav-element.js';
+import { MainElement } from '../components/main-element.js';
+import { AboutElement } from '../components/about-element.js';
 
 
 const outlet = document.getElementById('outlet');
-export const router = new Router(outlet);
+const router = new Router(outlet);
 
 router.setRoutes([
   {path: '/', component: 'nav-element', children: [
