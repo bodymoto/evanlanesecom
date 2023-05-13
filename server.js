e790a0e7e47abd6b1b847d4bd8924acd2988a9ea
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-05-13 23:48:18 +0700

    new: image route

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const port = 8000;
 
 app.use("/dist", express.static(path.resolve(__dirname, "dist")));
 
-// video source
+// video
 app.get("/video/pexels-lam-loi-3492325-1280x720-31fps.mp4", (req, res) => {
   res.sendFile(path.join(__dirname, "/video/pexels-lam-loi-3492325-1280x720-31fps.mp4"));
+});
+
+// images
+app.get("/img/colo.png", (req, res) => {
+  res.sendFile(path.join(__dirname, "/img/colo.png"));
 });
 
 
