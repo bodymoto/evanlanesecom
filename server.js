b93842b322067bd2d87a4df6bfe8a5af0b1fc9bf
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-05-18 23:58:19 +0700

    refactored routes

--- server.js
+++ server.js
@@ -8,21 +8,15 @@ const port = 8000;
 app.use("/dist", express.static(path.resolve(__dirname, "dist")));
 
 // video
-app.get("/video/pexels-lam-loi-3492325-1280x720-31fps.mp4", (req, res) => {
-  res.sendFile(path.join(__dirname, "/video/pexels-lam-loi-3492325-1280x720-31fps.mp4"));
+app.get("/video/:video", (req, res) => {
+  const video = req.params.video;
+  res.sendFile(path.join(__dirname, "/video/" + video));
 });
 
 // images
-app.get("/img/colo.png", (req, res) => {
-  res.sendFile(path.join(__dirname, "/img/colo.png"));
-});
-
-app.get("/img/bigblue.png", (req, res) => {
-  res.sendFile(path.join(__dirname, "/img/bigblue.png"));
-});
-
-app.get("/img/got-dance.gif", (req, res) => {
-  res.sendFile(path.join(__dirname, "/img/got-dance.gif"));
+app.get("/img/:image", (req, res) => {
+  const image = req.params.image;
+  res.sendFile(path.join(__dirname, "/img/" + image));
 });
 
 // catch-all
