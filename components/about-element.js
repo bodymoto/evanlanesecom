cffdf9d1f1a08f96395f678629dabe31a5be9414
Boshi <69194793+boshimoto@users.noreply.github.com>
2023-05-18 23:57:09 +0700

    new ideas for dev

--- components/about-element.js
+++ components/about-element.js
@@ -1,5 +1,5 @@
 import {LitElement, html, css} from 'lit';
-import {TypingElement} from './typing-element.js';
+
 import {TyrionElement} from './tyrion-element.js';
 
 
@@ -30,6 +30,9 @@ export class AboutElement extends LitElement {
 		  	height: 100%;
 		  }
 
+
+
+
 		.tyrion {
 				border: 1px solid black;
 				width: 255px;
@@ -44,25 +47,38 @@ export class AboutElement extends LitElement {
 		super();
 	}
 
+	handleClick() {
+		const input = this.shadowRoot.querySelector('input');
+		const empty = this.shadowRoot.querySelector('#add-here');
+
+		//reset
+		input.value = "";
+	}
+
 	render() {
 		return html`
 		<main>
 			<div id="heading">
 				<div id="intro">
-					<typing-element></typing-element>
-					<p></p>
+					<p>i invented B.I. or believable intelligence. its 100% secure & the best part is B.I. is located right on this device. Go ahead, ask B.I. anything!</p>
+					<p>...</p>
+					<p>B.I. is typing...</p>
 				</div>
 
 				<div id="image">
 					<img src="../img/bigblue.png" alt="An abstract self-portrait." />
 				</div>
 			</div>
+			<input type="text" maxlength="50" size="50"/>
+			<button @click=${this.handleClick} type="submit">ask question</button>
+			<div id="add-here">add elements here</div>
+			<p>B.I. runs locally meaning your device is hard at work processing a huge amount of bytes. Something like one thousand or even one million bytes! Its worth the wait. Any day now...</p>
 
-			<div class="tyrion">
-				<tyrion-element></tyrion-element>
-			</div>
 		</main>
 		`;
+			// <div class="tyrion">
+			// 	<tyrion-element></tyrion-element>
+			// </div>
 	}
 }
 
